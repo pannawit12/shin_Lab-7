e451fa2bc28dce612f6d1e587b329e25295a2ced
Pannawit12 <pannawit_se@cmu.ac.th>
2022-12-13 15:50:57 +0700

    เขียนโปรแกรม

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,14 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
+	double g;
+	int count=0;
+    cout << "Please input GPA: ";
+	cin >> g;
+	while(count<10){
+		if (gpa[count]>=g) cout << id[count] << " " << name[count] << " " << gpa[count] << endl;
+		count++;
+	}
 		
 	return 0;
 }
